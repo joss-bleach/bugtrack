01ee48e87e5d763091d09e2d07f6de0d3ef3d217
Joss Bleach <jossbleach@MacBook-Pro.local>
2020-06-10 18:05:24 +0100

    Database set up and user registration functionality added.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const connectDb = require("./config/db");
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+connectDb();
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}.`));
---
+++ routes/api/users.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../../models/User");
+
+// ROUTE - POST /api/users
+// DESC - Register a User
+// ACCESS - Public
+router.post(
+  "/",
+  [
+    check("firstName", "Please enter your first name.").not().isEmpty(),
+    check("lastName", "Please enter your last name").not().isEmpty(),
+    check("email", "Please enter a valid email address.").isEmail(),
+    check("email", "Please enter your email address.").not().isEmpty(),
+    check(
+      "password",
+      "Your password must contain at least 6 characters."
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    // Check for errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { firstName, lastName, email, password } = req.body;
+
+      // Check to see if user already exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists." }] });
+      }
+
+      // Create user instance
+      user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user
+      await user.save();
+
+      // Configure jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign jwt
+      jwt.sign(
+        payload,
+        config.get("secretOr"),
+        { expiresIn: config.get("jwtExpiry") },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
